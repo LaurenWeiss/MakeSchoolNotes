f9655c63975149d1a3374467da0a66306b93d7e3
LaurenWeiss <lolow123@comcast.net>
2017-06-26 10:04:38 -0700

    Finished Notes App Project

---
+++ MakeSchoolNotes/MakeSchoolNotes/Models/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  MakeSchoolNotes
+//
+//  Created by Lauren Weiss on 6/22/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
